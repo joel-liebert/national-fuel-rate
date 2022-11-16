2e52228d20e2939c04d20dd597adc0b4fae090c2
joel-liebert <jliebert@freightwaves.com>
2022-11-16 10:12:40 -0500

    finished the analysis

--- analysis/national-fuel-rate-analysis.R
+++ analysis/national-fuel-rate-analysis.R
@@ -20,7 +20,89 @@ query1 =
     and peer_group='market'
     and date='2022-11-06'"
 
+query2 = 
+  "with sums as (
+      select
+        data_timestamp + 1 as data_timestamp
+        ,mode
+        ,cast( substr(od_pair, 1, 3) as int64 ) as origin_zip3
+        ,cast( substr(od_pair, 5, 3) as int64 ) as dest_zip3
+        ,substr(od_pair, 1, 1) as origin_zip1
+        ,substr(od_pair, 5, 1) as dest_zip1
+        ,effective_fuel_charge
+        ,effective_miles
+      from `freightwaves-data-factory.output_trucking_rates.contract_output_history` tablesample system (25 percent)
+      where data_timestamp = '2022-11-13'
+    ),
+    PADD as (
+      select
+        data_timestamp
+        ,mode
+        ,origin_zip1
+        ,dest_zip1
+        ,case 
+          when origin_zip3  > 888 and dest_zip3  > 888 then 'full PADD5'
+          when origin_zip3  > 888 or  dest_zip3  > 888 then 'half PADD5'
+          when origin_zip3 <= 888 and dest_zip3 <= 888 then 'not PADD5'
+          end as PADD_type
+        ,effective_fuel_charge
+        ,effective_miles
+      from sums
+    ),
+    regions as (
+      select
+        data_timestamp
+        ,mode
+        ,PADD_type
+        ,case 
+          when origin_zip1 in ('0','1','2') then 'atlantic'
+          when origin_zip1 in ('3','7')     then 'south'
+          when origin_zip1 in ('4','5','6') then 'midwest'
+          when origin_zip1 in ('8','9')     then 'west'
+          end as orig_region
+        ,case 
+          when dest_zip1 in ('0','1','2') then 'atlantic'
+          when dest_zip1 in ('3','7')     then 'south'
+          when dest_zip1 in ('4','5','6') then 'midwest'
+          when dest_zip1 in ('8','9')     then 'west'
+          end as dest_region
+        ,effective_fuel_charge
+        ,effective_miles
+      from PADD
+    ),
+    lane_regions as (
+      select
+        data_timestamp
+        ,mode
+        ,PADD_type
+        ,concat( greatest(orig_region, dest_region), '-', least(orig_region, dest_region) ) as lane_region
+        ,effective_fuel_charge
+        ,effective_miles
+      from regions
+    ),
+    LOH as (
+      select
+        data_timestamp
+        ,mode
+        ,PADD_type
+        ,lane_region
+        ,case 
+          when effective_miles < 100 then 'city'
+          when effective_miles < 250 then 'short'
+          when effective_miles < 500 then 'mid'
+          when effective_miles < 750 then 'tweener'
+          when effective_miles < 1000 then 'long'
+          else 'extra-long'
+          end as LOH_type
+        ,round( safe_divide(effective_fuel_charge, effective_miles), 2 ) as fuel_rpm
+      from lane_regions
+    )
+    
+    select * from LOH
+    order by data_timestamp, PADD_type, lane_region, LOH_type, fuel_rpm"
+
 contract_df <- dbGetQuery(bqcon, query1)
+contract_df2 <- dbGetQuery(bqcon, query2)
 
 #---- create estimated boundaries ----
 # set up matrix
@@ -47,6 +129,10 @@ fuel_charge_vector <- c(low=min(combos$fuel_charge_per_miles),
 fuel_surcharge_vector <- c(low=min(combos$fuel_surcharge_per_miles),
                            mid=mean(combos$fuel_surcharge_per_miles),
                            high=max(combos$fuel_surcharge_per_miles)) %>% print()
+
+temp_FSC_vector <- c(low=doe_FW_ranges[doe_FW_ranges$ds=='2022-11-14',]$value[1],
+                     mid=doe_FW_ranges[doe_FW_ranges$ds=='2022-11-14',]$value[2],
+                     high=doe_FW_ranges[doe_FW_ranges$ds=='2022-11-14',]$value[3])
 
 #---- visualize distribution of fuel_rpm ----
 fuel_rpm_distribution_plotter <- function(data, hline, facet_wrap) {
@@ -88,9 +174,80 @@ rpm_df <- contract_df %>%
          LOH = factor(LOH, levels=c('city','short','mid','tweener','long','extra-long')),
          fuel_rpm = round( effective_fuel_charge/effective_miles, 2 ))
 
-fuel_rpm_distribution_plotter(rpm_df, fuel_surcharge_vector, 'LOH') +
+contract_df2 <- contract_df2 %>% 
+  mutate(LOH_type = factor(LOH_type, levels=c('city','short','mid','tweener','long','extra-long')))
+
+fuel_rpm_distribution_plotter(rpm_df, fuel_surcharge_vector, 'Mode ') +
   ggtitle("Contract Fuel Rates Plotted Against Low, Mid, & High Expectations") +
   labs(x='Mode', y='Fuel RPM')
+
+mode_boxplot <- contract_df2 %>% 
+  ggplot(aes(mode, fuel_rpm)) +
+  geom_boxplot() +
+  geom_hline(yintercept=temp_FSC_vector) +
+  theme_classic() +
+  ggtitle("Mode Fuel Rates vs Projected Range (2022-11-14)") +
+  labs(x=' ', y='Fuel RPM')
+mode_boxplot
+
+PADD_boxplot <- contract_df2 %>% 
+  ggplot(aes(mode, fuel_rpm)) +
+  geom_boxplot() +
+  geom_hline(yintercept=temp_FSC_vector) +
+  facet_wrap('PADD_type') +
+  theme_classic() +
+  ggtitle("PADD Fuel Rates vs Projected Range (2022-11-14)") +
+  labs(x=' ', y='Fuel RPM')
+PADD_boxplot
+
+region_boxplot <- contract_df2 %>% 
+  ggplot(aes(mode, fuel_rpm)) +
+  geom_boxplot() +
+  geom_hline(yintercept=temp_FSC_vector) +
+  facet_wrap('lane_region') +
+  theme_classic() +
+  ggtitle("Region Fuel Rates vs Projected Range (2022-11-14)") +
+  labs(x=' ', y='Fuel RPM')
+region_boxplot
+
+LOH_boxplot <- contract_df2 %>% 
+  ggplot(aes(mode, fuel_rpm)) +
+  geom_boxplot() +
+  geom_hline(yintercept=temp_FSC_vector) +
+  facet_wrap('LOH_type') +
+  theme_classic() +
+  ggtitle("LOH Fuel Rates vs Projected Range (2022-11-14)") +
+  labs(x=' ', y='Fuel RPM')
+LOH_boxplot
+
+
+PADD_boxplot_no_mode <- contract_df2 %>% 
+  ggplot(aes(PADD_type, fuel_rpm)) +
+  geom_boxplot() +
+  geom_hline(yintercept=temp_FSC_vector) +
+  theme_classic() +
+  ggtitle("PADD Fuel Rates vs Projected Range (2022-11-14)") +
+  labs(x=' ', y='Fuel RPM')
+PADD_boxplot_no_mode
+
+region_boxplot_no_mode <- contract_df2 %>% 
+  ggplot(aes(lane_region, fuel_rpm)) +
+  geom_boxplot() +
+  geom_hline(yintercept=temp_FSC_vector) +
+  theme_classic() +
+  ggtitle("Region Fuel Rates vs Projected Range (2022-11-14)") +
+  labs(x=' ', y='Fuel RPM') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+region_boxplot_no_mode
+
+LOH_boxplot_no_mode <- contract_df2 %>% 
+  ggplot(aes(LOH_type, fuel_rpm)) +
+  geom_boxplot() +
+  geom_hline(yintercept=temp_FSC_vector) +
+  theme_classic() +
+  ggtitle("LOH Fuel Rates vs Projected Range (2022-11-14)") +
+  labs(x=' ', y='Fuel RPM')
+LOH_boxplot_no_mode
 
 #---- history ----
 # query2 = 
--- analysis/rpm-vs-projections-ts.R
+++ analysis/rpm-vs-projections-ts.R
@@ -1,21 +1,24 @@
 #---- import packages ----
 library(googleCloudStorageR)
 library(bigrquery)
+library(DBI)
+library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(zoo)
+library(lubridate)
 
 source('./utils.R')
 
 #---- load data ----
-doe_FW <- response_pull(data_to_forecast='DOE', type='ticker')
+doe_FW <- response_pull(data_to_forecast='DOE', type='ticker', start_date='2018-10-27')
 
 bqcon <- dbConnect(
   bigrquery::bigquery(),
   project = "freightwaves-data-factory"
 )
 
-query1 = 
+query_main = 
   "with sums as (
       select
         data_timestamp + 1 as data_timestamp
@@ -23,7 +26,7 @@ query1 =
         ,sum(effective_miles) as effective_miles_sum
         ,sum(effective_fuel_charge) as effective_fuel_charge_sum
       from `freightwaves-data-factory.output_trucking_rates.contract_output_history`
-      where data_timestamp > '2019-01-01'
+      where data_timestamp > '2018-10-27'
       group by data_timestamp, mode
     ),
     final as (
@@ -36,7 +39,140 @@ query1 =
     select * from final
     order by data_timestamp, mode"
 
-contract_df <- dbGetQuery(bqcon, query1)
+query_PADD = 
+  "with sums as (
+      select
+        data_timestamp + 1 as data_timestamp
+        ,cast( substr(od_pair, 1, 3) as int64 ) as origin_zip3
+        ,cast( substr(od_pair, 5, 3) as int64 ) as dest_zip3
+        ,round( safe_divide(effective_fuel_charge, effective_miles), 2 ) as fuel_rpm
+      from `freightwaves-data-factory.output_trucking_rates.contract_output_history`
+      where data_timestamp > '2018-10-27'
+    ),
+    PADD as (
+      select
+        data_timestamp
+        ,case 
+          when origin_zip3  > 888 and dest_zip3  > 888 then 'full PADD5'
+          when origin_zip3  > 888 or  dest_zip3  > 888 then 'half PADD5'
+          when origin_zip3 <= 888 and dest_zip3 <= 888 then 'not PADD5'
+          end as PADD_type
+        ,fuel_rpm
+      from sums
+    ),
+    median as (
+      select
+        data_timestamp, PADD_type
+        ,percentile_cont(fuel_rpm, 0.5) over(partition by data_timestamp, PADD_type) as fuel_rpm
+      from PADD
+    ),
+    final as (
+      select
+        data_timestamp, PADD_type
+        ,max(fuel_rpm) as fuel_rpm
+      from median
+      group by data_timestamp, PADD_type
+    )
+    
+    select * from final
+    order by data_timestamp, PADD_type"
+
+query_region = 
+  "with sums as (
+      select
+        data_timestamp + 1 as data_timestamp
+        ,substr(od_pair, 1, 1) as origin_zip3
+        ,substr(od_pair, 5, 1) as dest_zip3
+        ,round( safe_divide(effective_fuel_charge, effective_miles), 2 ) as fuel_rpm
+      from `freightwaves-data-factory.output_trucking_rates.contract_output_history`
+      where data_timestamp > '2018-10-27'
+    ),
+    regions as (
+      select
+        data_timestamp
+        ,case 
+          when origin_zip3 in ('0','1','2') then 'atlantic'
+          when origin_zip3 in ('3','7')     then 'south'
+          when origin_zip3 in ('4','5','6') then 'midwest'
+          when origin_zip3 in ('8','9')     then 'west'
+          end as orig_region
+        ,case 
+          when dest_zip3 in ('0','1','2') then 'atlantic'
+          when dest_zip3 in ('3','7')     then 'south'
+          when dest_zip3 in ('4','5','6') then 'midwest'
+          when dest_zip3 in ('8','9')     then 'west'
+          end as dest_region
+        ,fuel_rpm
+      from sums
+    ),
+    lane_regions as (
+      select
+        data_timestamp
+        ,concat( greatest(orig_region, dest_region), '-', least(orig_region, dest_region) ) as lane_region
+        ,fuel_rpm
+      from regions
+    ),
+    median as (
+      select
+        data_timestamp, lane_region
+        ,percentile_cont(fuel_rpm, 0.5) over(partition by data_timestamp, lane_region) as fuel_rpm
+      from lane_regions
+    ),
+    final as (
+      select
+        data_timestamp, lane_region
+        ,max(fuel_rpm) as fuel_rpm
+      from median
+      group by data_timestamp, lane_region
+    )
+    
+    select * from final
+    order by data_timestamp, lane_region"
+
+query_LOH = 
+  "with sums as (
+      select
+        data_timestamp + 1 as data_timestamp
+        ,effective_miles
+        ,round( safe_divide(effective_fuel_charge, effective_miles), 2 ) as fuel_rpm
+      from `freightwaves-data-factory.output_trucking_rates.contract_output_history`
+      where data_timestamp > '2018-10-27'
+    ),
+    LOH as (
+      select
+        data_timestamp
+        ,case 
+          when effective_miles < 100 then 'city'
+          when effective_miles < 250 then 'short'
+          when effective_miles < 500 then 'mid'
+          when effective_miles < 750 then 'tweener'
+          when effective_miles < 1000 then 'long'
+          else 'extra-long'
+          end as LOH_type
+        ,fuel_rpm
+      from sums
+    ),
+    median as (
+      select
+        data_timestamp, LOH_type
+        ,percentile_cont(fuel_rpm, 0.5) over(partition by data_timestamp, LOH_type) as fuel_rpm
+      from LOH
+    ),
+    final as (
+      select
+        data_timestamp, LOH_type
+        ,max(fuel_rpm) as fuel_rpm
+      from median
+      group by data_timestamp, LOH_type
+    )
+    
+    select * from final
+    order by data_timestamp, LOH_type"
+
+main_df   <- dbGetQuery(bqcon, query_main)
+PADD_df   <- dbGetQuery(bqcon, query_PADD)
+region_df <- dbGetQuery(bqcon, query_region)
+LOH_df    <- dbGetQuery(bqcon, query_LOH)
 
 #---- create low, mid, and high projections ----
 fc  <- list(low_fc=1,  mid_fc=1.5, high_fc=2)
@@ -52,15 +188,52 @@ doe_FW_ranges <- doe_FW %>%
   tidyr::pivot_longer(cols=c('low_fsc', 'mid_fsc', 'high_fsc'), names_to='rate', values_to='value')
 
 #---- combine data ----
-combined_df <- doe_FW_ranges %>% 
-  left_join(contract_df, by=c('ds'='data_timestamp'))
+combined_main_df   <- doe_FW_ranges %>% 
+  left_join(main_df,   by=c('ds'='data_timestamp')) %>% 
+  # right_join(todd_df,  by=c('ds'='DATE')) %>% 
+  mutate(todd_FSC = (y-1.2)/6,
+         HEB_FSC = ceiling( ((y-1.22)/6) *100 )/100,
+         HBC_FSC = ceiling( ((y-1.15)/5) *100 )/100,
+         Genpak_FSC = ceiling( ((y-1.11)/6) *100 )/100)
 
+combined_PADD_df   <- doe_FW_ranges %>% 
+  left_join(PADD_df,   by=c('ds'='data_timestamp'))
+
+combined_region_df <- doe_FW_ranges %>% 
+  left_join(region_df, by=c('ds'='data_timestamp'))
+
+combined_LOH_df    <- doe_FW_ranges %>% 
+  left_join(LOH_df,    by=c('ds'='data_timestamp'))
 #---- plot data ----
-combined_plot <- combined_df %>%
-  ggplot(aes(ds, value, fill=rate)) +
-  geom_line() +
-  geom_line(aes(ds, fuel_rpm, color=mode)) +
-  theme_classic() +
-  labs(x='', y='Fuel RPM') +
-  ggtitle('Contract Fuel Rates vs Projected Range (based on ten-week rolling average of DOE.USA)')
-combined_plot
+fuel_rate_plotter <- function(data, rate_type) {
+
+  plot <- data %>%
+    ggplot(aes(ds, value, fill=rate)) +
+    geom_line() +
+    theme_classic() +
+    labs(x='', y='Fuel RPM') +
+    ggtitle(paste0(rate_type, ' Fuel Rates vs Projected Range (based on ten-week rolling average of DOE.USA)'))
+
+  return(plot)
+
+}
+
+main_plot <- combined_main_df %>%
+  fuel_rate_plotter('Mode') +
+  geom_line(aes(ds, fuel_rpm, color=mode))
+main_plot
+
+PADD_plot <- combined_PADD_df %>% 
+  fuel_rate_plotter('PADD') +
+  geom_line(aes(ds, fuel_rpm, color=PADD_type))
+PADD_plot
+
+region_plot <- combined_region_df %>% 
+  fuel_rate_plotter('Region') +
+  geom_line(aes(ds, fuel_rpm, color=lane_region))
+region_plot
+
+LOH_plot <- combined_LOH_df %>% 
+  fuel_rate_plotter('LOH') +
+  geom_line(aes(ds, fuel_rpm, color=LOH_type))
+LOH_plot
